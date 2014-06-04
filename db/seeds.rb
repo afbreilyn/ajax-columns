2b9449ca451313dff4181faf5eb7f8f77c677c7d
afbresley <afbresley@gmail.com>
2014-06-04 11:25:58 -0400

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Sentence.create!({ body: "\"And those who were seen dancing were thought to be insane by those who could not hear the music.\" - Friedrich Nietzsche", is_left: true })
+Sentence.create!({ body: "\"Jetpacking should be an olympic sport!\" - Burnie Burns", is_left: true })
+Sentence.create!({ body: "\"There is an art, or, rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.\" - Douglas Adams", is_left: true })
+Sentence.create!({ body: "\"[My desire is to] stir up, through the production of the music, a sense of social conditions in which we live and of how these might be changed.\" - Christian Wolff", is_left: true })
+Sentence.create!({ body: "\"You can either practice being right or practice being kind\" - Anne Lamott", is_left: true })
+Sentence.create!({ body: "\"I think... I feel good about it.\" - Marvin", is_left: true })
+Sentence.create!({ body: "\"A concept is a brick. It can be used to build a courthouse of reason. Or it can be thrown through the window.\" - Gilles Deleuze and Félix Guattari", is_left: true })
+Sentence.create!({ body: "\"Valar morghulis, valar dohaeris\" - the Faceless Men", is_left: true })
